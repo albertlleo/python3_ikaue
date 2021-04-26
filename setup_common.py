d15790a98789c200639bb0421182663c4ccd466d
albertlleo <albert.lleo3@gmail.com>
2021-04-26 16:20:24 +0200

    latest

--- setup_common.py
+++ setup_common.py
@@ -4,7 +4,7 @@ import NLP_ikaue
 def define_args():
     install_requirements = ['beautifulsoup4>=4.9.1','wheel>=0.36.2','googlesearch-python>=2020.0.2',
                             'bs4>=0.0.1','gapic-google-cloud-language-v1>=0.15.3',
-                            'google-api-lib>=1.26.2','google-api-python-client>=2.0.2','urllib3>=1.25.9',
+                            'google-api-python-client>=2.0.2','urllib3>=1.25.9',
                             'google-cloud-language>=2.0.0','pandas>=1.2.4','pytest>=6.2.3']
 
     test_requirements = ['pytest>=4.3,<4.4', 'pytest-cov>=2.7,<2.8']
